c07a23095660a67693692fc1c1f2d50641b227dd
Anwesh43 <anweshthecool0@gmail.com>
2023-04-16 22:42:59 +0530

    useStyle hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react'
+import {useState, useEffect, CSSProperties} from 'react'
 
 export const useAnimatedScale = (scGap : number  = 0.01, delay : number = 20) => {
     const [scale, setScale] = useState<number>(0)
@@ -40,5 +40,43 @@ export const useDimension = () => {
     return {
         w, 
         h
+    }
+}
+
+const sinify = (scale : number) : number => Math.sin(scale * Math.PI)
+
+export const useStyle = (w : number, h : number, scale : number)  => {
+    const position = 'absolute'
+    const background = 'purple'
+    const size : number = Math.min(w, h) / 8
+    return {
+        lineStyle() : CSSProperties {
+            const width : string = `${Math.min(w, h) / 60}px`
+            const height : string = `${size}px`
+            const top : string = `${(h / 2 - size) * sinify(scale)}px`
+            const left : string = `${w / 2 - Math.min(w, h) / 120}px`
+            return {
+                width, 
+                height, 
+                left, 
+                top, 
+                background, 
+                position 
+            }
+        }, 
+        barStyle() : CSSProperties {
+            const width : string = `${size}px`
+            const height : string = `${size}px`
+            const top : string = `${h / 2 - size / 2}px`
+            const left : string = `${(w / 2 - size / 2) * (1 + sinify(scale))}px`
+            return {
+                width, 
+                height,
+                left, 
+                top, 
+                background,
+                position 
+            }
+        }
     }
 }
